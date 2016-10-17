78d8c1495cc5e90f1c25bafafa3e3b945666e4a7
Rudolph Hernandez <rudolpharthur@gmail.com>
2016-10-17 10:26:35 -0700

    remove table footer

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -6,19 +6,14 @@
 @section('main')
 
 
-<table id="members-table" class="table table-striped table-bordered compact" cellspacing="0" width="100%" >
+<table id="members-table" class="table table-striped table-bordered " cellspacing="0" width="100%" >
     <thead>
       <tr>
         <th>Name</th>
         <th>Joined</th>
       </tr>
     </thead>
-	<tfoot>
-	  <tr>
-	    <th>Name</th>
-	    <th>Joined</th>
-	  </tr>
-	</tfoot>
+
     <tbody>
     </tbody>
   </table>
